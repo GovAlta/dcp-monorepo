8885967b4bf20f82f45123dfc0ad48626b2dbe5c
Ting Zuge <ting.zuge@gov.ab.ca>
2023-03-29 08:28:00 -0600

    chore: updating LICENSE and adding some documentation in README.md
    Also adding @astrojs/mdx plugin.

--- apps/dcp-guide/astro.config.mjs
+++ apps/dcp-guide/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
 export default defineConfig({
   outDir: '../../dist/apps/dcp-guide',
-  integrations: [react()],
+  integrations: [mdx(), react()],
 });
