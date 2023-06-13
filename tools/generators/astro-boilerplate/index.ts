11e4bafda5c77f40c5631ee6d4f56eae75b8838d
smettu007 <subbumettu007@gmail.com>
2023-06-13 15:26:01 -0600

    feat: added astro generator

---
+++ tools/generators/astro-boilerplate/index.ts
@@ -0,0 +1,29 @@
+import { Tree, formatFiles, installPackagesTask, generateFiles } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+import * as path from 'path';
+
+
+export default async function (host: Tree, schema: any) {
+  if(!schema?.title){
+    throw new Error("Missing title")
+  }
+  generateFiles(
+    // virtual file system
+    host,
+
+    // the location where the template files are
+    path.join(__dirname, 'openshift'),
+
+    // where the files should be generated
+    '.openshift',
+
+    // the variables to be substituted in the template
+    {
+      appName: schema.title,
+    }
+  );
+  await formatFiles(host);
+  return () => {
+    installPackagesTask(host);
+  };
+}
