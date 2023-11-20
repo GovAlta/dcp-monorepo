d0def5a5c8aab0a34dbd9764309e73e650b9543e
subbaramaiah mettu <subbumettu007@gmail.com>
2023-11-20 15:21:19 -0600

    feat: added details page and badges logic

---
+++ apps/common-capabilities/src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  GoAMicrositeHeader,
+  GoAAppHeader,
+  GoAHeroBanner,
+  GoAButton,
+  GoAButtonGroup,
+  GoAHeroBannerActions,
+} from '@abgov/react-components';
+
+const Header = ({ actionButtons }: any) => {
+  return (
+    <>
+      <GoAMicrositeHeader type="beta" />
+      <GoAAppHeader url="/" heading="Common capabilities" maxContentWidth="95%">
+        <a href="/">Home</a>
+        <a href="/contact/index.html">Contact</a>
+      </GoAAppHeader>
+      <GoAHeroBanner
+        heading="Common capabilities"
+        backgroundUrl=""
+        minHeight="20%"
+      >
+        Reusable services for building your custom apps
+        {actionButtons ? (
+          <GoAHeroBannerActions>
+            <GoAButtonGroup alignment="start">
+              <GoAButton
+                leadingIcon="arrow-back"
+                onClick={() => {
+                  console.log('yolo');
+                  window.location.href = '/';
+                }}
+              >
+                Back to services
+              </GoAButton>
+            </GoAButtonGroup>
+          </GoAHeroBannerActions>
+        ) : (
+          ''
+        )}
+      </GoAHeroBanner>
+    </>
+  );
+};
+
+export default Header;
