0a16866e607501584d1ea2f85e85a4dc3c5644dd
subbaramaiah mettu <subbumettu007@gmail.com>
2024-02-13 17:08:35 -0700

    feat: added collapsible fields for filters

---
+++ apps/common-capabilities/src/pages/home/config.ts
@@ -0,0 +1,18 @@
+// the names in the filterList and the keys in the defaultState objects should match
+export const defaultState = {
+  selectedFilters: {
+    Environment: [],
+    Language: [],
+    Keywords: [],
+    Status: [],
+    FunctionalGroup: [],
+  }
+};
+
+export const filtersList = [
+  'Environment',
+  'Language',
+  'Keywords',
+  'Status',
+  'FunctionalGroup',
+];
