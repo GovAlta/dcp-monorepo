5c96563b375c283d0afda2c9e4264a88012cfb31
Steve Rozeboom <steve.rozeboom@gov.ab.ca>
2024-02-26 16:17:07 -0700

    Update to CSV2JSON filters

--- apps/common-capabilities/data/CSV2JSON.py
+++ apps/common-capabilities/data/CSV2JSON.py
@@ -1,29 +1,77 @@
+import sys
 import csv
 import json
 import os
+import re
 from datetime import datetime
-# To get the URL page name
-# from bs4 import BeautifulSoup   #pip install beautifulsoup4
-# import requests                 # pip install requests 
 from urllib.parse import urlparse
 
+try:
+    if len(sys.argv) > 3:
+        raise Exception('Too many parameters')    
+    if len(sys.argv) > 1:
+        DevProdArg = sys.argv[1].upper()
+    else:
+        DevProdArg = ''
+    if len(sys.argv) > 2:
+        FldArg = sys.argv[2]
+    else:
+        FldArg = ''
+    if len(DevProdArg) > 1:
+        raise Exception('use either P or D')
+except Exception as inst:
+    print( inst)
+    print( "Usage: python csv2json.py <[P,D]> <output path>")
+    sys.exit(1)
 
-DevMode = False     # <<<======== IMPORTANT ===============
 
-if not DevMode:
+ProductionData = False     # <<<======== IMPORTANT - Default ===============
+if (DevProdArg != ''):
+    ProductionData = DevProdArg == 'P'
+
+if ProductionData:
     CSV_fileNames = ['CommonCapabilities']
     outputDirectories = ['..\\src\\content\\']
 else:
     CSV_fileNames = ['CommonCapabilities','CommonCapabilitiesSamples']
-    outputDirectories = ['..\\..\\..\\..\\..\\ReactJS\\list\public\\']
+    outputDirectories = ['..\\..\\..\\..\\..\\ReactJS\\list\\build\\','..\\..\\..\\..\\..\\ReactJS\\list\public\\']
+
 
 CSV_fileDir = '.\\'
-if not os.path.exists(CSV_fileDir + 'CommonCapabilitiesFields.csv'):
-    CSV_fileDir = '.\\data\\' 
+fldArray = 'data','common-capabilities','apps', ''   # need the '' for for loop
+tmp = ''
+foundPath = ''
+for addThis in fldArray:
+    if os.path.exists('.'+ tmp + '\CommonCapabilitiesFields.csv'):        
+        foundPath = '.'+ tmp + '\\'
+        break
+    tmp = '\\' + addThis + tmp
 
-outputDirectories = [CSV_fileDir + string for string in outputDirectories]
-outputDirectories += [CSV_fileDir]
+if foundPath == '':
+    print('\n-----------------\nCould not find files\n----------------\n')
+    sys.exit()
 
+
+outputDirectories = [foundPath + string for string in outputDirectories]
+CSV_fileDir = foundPath
+
+if (FldArg != ''):
+    if FldArg[-1] != '\\':
+        FldArg += '\\'    
+    outputDirectories = [FldArg]
+
+haveOutput = False
+for folderName in outputDirectories:
+    if os.path.exists(folderName):
+        haveOutput = True
+        break
+if not haveOutput:
+    print('\n-----------------\nCould not find output folder:', outputDirectories )
+    sys.exit()
+
+
+# Finished checks, I've got everything I need to continue...
+#=================================================
 
 def replace_special_characters(text):
     return text.replace("’", "'").replace("\u200b", " ").replace("\u00a0", " ").strip()
@@ -54,22 +102,12 @@ def getPageNameFromURL(url):
     parsed_url = urlparse(url)
     path = parsed_url.path    
     page_name = path.split("/")[-1]    
-    # print(url)    # print(path.split("/"))    # print(parsed_url)    # print(page_name)
-    
     if page_name == "":
         page_name = parsed_url.netloc
     elif len(page_name) < 5:
         page_name = parsed_url.netloc + " " + page_name
 
     return page_name.replace("%20", " ")
-
-    # response = requests.get(url)                            # Make a request to the URL
-    # if response.status_code == 200:                         # Check if the request was successful (status code 200)
-    #     soup = BeautifulSoup(response.text, 'html.parser')  # Parse the HTML content of the page        
-    #     title_tag = soup.find('title')                      # Extract the title tag content
-    #     if title_tag:
-    #         return title_tag.text.strip()
-    # return None     # If the request was not successful or title not found, return None
 
 def linkList(text):    
     links = []    
@@ -105,6 +143,17 @@ def createContact(type,url,descr,email,phone, provider):
 
 # ============================================================================================
 
+if DevProdArg == '':
+    colorBlack = '\033[1;30m'
+    colorRed = '\033[1;31m'
+    colorGreen = '\033[0;32m'
+    colorReset = '\033[0m'
+else:
+    colorBlack = ''
+    colorRed = ''
+    colorGreen = ''
+    colorReset = ''  
+
 filterData = []
 with open(CSV_fileDir + 'CommonCapabilitiesFields.csv', 'r', encoding='utf-8-sig') as csvfile:
     csvreader = csv.DictReader(csvfile)
@@ -116,9 +165,10 @@ with open(CSV_fileDir + 'CommonCapabilitiesFields.csv', 'r', encoding='utf-8-sig
 
 data = []
 id_counter = 0
-if DevMode: devt = 'in DEVELOPMENT MODE'
-else: devt = ''  
-print('\n----[ Create JSON files '+ devt + ' ]------------\nWorking directory: '+ CSV_fileDir +'\nInput: ' + 'CommonCapabilitiesFields.csv')
+if not ProductionData: devt = 'DEVELOPMENT'
+else: devt = 'Production'  
+print('\n' + colorBlack + '------[ Create JSON files for ' + colorRed + devt + colorBlack + ' ]---------\n' + colorGreen +'Working directory: '
+      + CSV_fileDir +'\nInput: ' + 'CommonCapabilitiesFields.csv')
 for fileName in CSV_fileNames:    
     if os.path.exists(CSV_fileDir + fileName + '.csv'):
         print('Input: '+ fileName + '.csv')   
@@ -133,11 +183,9 @@ for fileName in CSV_fileNames:
                     
                     filterText = ''
                     for row2 in filterData:
-                        fn = row2["FieldName"]       # int, text, textArray, urlArray, contactList                       
-
-                        if row2["isFilter"] == True and  row2["dataType"] != "int" and csv_row[fn] != '':
-                            filterText += ',' + csv_row[fn]
-                        
+                        fn = row2["FieldName"]       # int, text, textArray, urlArray, contactList                                
+                        if row2["Filter"] != 'No' and  row2["dataType"] != "int" and csv_row[fn] != '':
+                            filterText += ',' + csv_row[fn]                        
 
                         if   row2["dataType"] == "urlArray":    csv_row[fn] = linkList(csv_row[fn])                        
                         elif row2["dataType"] == "textArray":   csv_row[fn] = asArray(csv_row[fn])
@@ -172,6 +220,68 @@ for fileName in CSV_fileNames:
 
                     data.append(csv_row)
 
+# ***************** Vote on filter text *********************                     
+def shorten(text): return re.sub('[._ -\\/]', '', text).lower().strip()
+wordCounts = {}
+needEdit = {}
+modifiedRecords = []
+def wordVote(val):
+    if val in wordCounts:        wordCounts[val] += 1
+    else:        wordCounts[val] = 1
+
+def needsEdit(val):
+    short = shorten(val)
+    if short != '' and short in needEdit and needEdit[short] != val:
+        return needEdit[short]
+    else:
+        return {}
+
+for row2 in filterData:
+    fn = row2["FieldName"]
+    if row2["Filter"] != 'No':        
+        for dataRow in data:
+            if row2["dataType"] == 'text':
+                if dataRow[fn] != '':                   
+                    wordVote(dataRow[fn])
+            elif row2["dataType"] == 'textArray':
+                    for val in dataRow[fn]:
+                        wordVote(val)
+string_counts_with_group = [{'string': key, 'count': value, 'group': shorten(key)} for key, value in wordCounts.items()]
+groups = set(item['group'] for item in string_counts_with_group)
+# def items_by_group(group): return [item for item in string_counts_with_group if item['group'] == group]
+
+for grp in groups:    
+    itemGroup = [item for item in string_counts_with_group if item['group'] == grp] # items_group_A = items_by_group(grp)
+    if len(itemGroup) > 1:        
+        gstr = ''
+        cnt = 0
+        grp = itemGroup[0]['group']
+        for item in itemGroup:
+            if item['count'] > cnt:
+                cnt = item['count']
+                gstr = item['string']
+        needEdit[grp] = gstr
+        
+for row2 in filterData:
+    fn = row2["FieldName"]
+    if row2["Filter"] != 'No':
+        for dataRow in data:
+            if row2["dataType"] == 'text':
+                updated = needsEdit(dataRow[fn])
+                if updated:
+                    modifiedRecords.append([dataRow['ServiceName'],fn,dataRow[fn],updated])
+                    dataRow[fn] = updated
+
+            elif row2["dataType"] == 'textArray':
+                    for val in dataRow[fn]:
+                        updated = needsEdit(val)
+                        if updated:
+                            i = dataRow[fn].index(val)
+                            modifiedRecords.append([dataRow['ServiceName'],fn,dataRow[fn][i],updated])                            
+                            dataRow[fn][i] = updated
+# ***************** END - Vote on filter text *********************
+
+
 parsed_dates = [datetime.strptime(item['LastUpdated'], '%m/%d/%Y') for item in data]
 mostRecentService = max(parsed_dates).strftime("%m/%d/%Y")
 
@@ -190,4 +300,11 @@ for folderName in outputDirectories:
             jsonfile.write(json.dumps(data2, indent=4))
         print('Output: ' + folderName + 'datastore.json')
 
-print('------- '+ str(id_counter) + ' total records -------------\n')
+if len(modifiedRecords) > 0:
+    modifiedRecords.sort()
+    with open(CSV_fileDir + 'modifiedRecords.txt', 'w') as file:
+        for item in modifiedRecords:
+            file.write("%s\n" % item)
+    print('AutoFix: '+CSV_fileDir+'modifiedRecords.txt contains '+str(len(modifiedRecords))+' modified records')
+
+print(colorBlack + '------- '+ str(id_counter) + ' total records -------------'+ colorReset + '\n')
