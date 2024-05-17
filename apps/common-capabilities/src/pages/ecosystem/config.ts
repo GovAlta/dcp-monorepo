05d45d47049b9003bde397fae7534b5466ba4403
subbaramaiah mettu <subbumettu007@gmail.com>
2024-05-17 13:46:53 -0600

    feat: added about and eco system pages

---
+++ apps/common-capabilities/src/pages/ecosystem/config.ts
@@ -0,0 +1,126 @@
+export const DDDDirectory = [
+  {
+    provider: 'Digital Design and Delivery',
+    name: 'Sheldon Bauld',
+    designation: 'Director, Front-End / UX Design',
+  },
+  {
+    provider: 'Alberta Digital Services Platform (ADSP)',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Design System',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Data and Content Management',
+    name: 'Erin Zybart',
+    designation: 'Director, IM Projects',
+  },
+  {
+    provider: 'Data Services',
+    name: 'Dony Alex',
+    designation: 'Director, Data Centre of Excellence',
+  },
+  {
+    provider: 'Cybersecurity',
+    name: 'Michael Schwab',
+    designation: 'Director, Application and Product Security',
+  },
+  {
+    provider: 'Digital Service Standards',
+    name: 'Heather Archibald',
+    designation: 'Director, Service Standard',
+  },
+];
+
+export const enterpriseDirectory = [
+  {
+    provider: 'Alberta.ca',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: '1GX',
+    name: 'John Faustino',
+    designation: 'Director, Integration Services',
+  },
+  {
+    provider: 'CloudOps',
+    name: 'Ankita Bakshi',
+    designation: 'Director, CloudOps',
+  },
+  {
+    provider: 'Enterprise Solutions',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+];
+
+export const portfolioPlatformDirectory = [
+  {
+    provider: 'Seniors, Community and Social Services (SCSS) Platform',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Children and Family Services (CFS) Platform',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Justice Digital',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Advanced Education (AE) Digital',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Wildfire Platform',
+    name: 'N/A',
+    designation: 'N/A',
+  },
+  {
+    provider: 'Platforms - Common Data services',
+    name: 'Caleb Kwok',
+    designation: 'Director, Common Data Services',
+  },
+  {
+    provider: 'Platforms - Identity and Access Management',
+    name: 'Robert Salloum',
+    designation: 'Director, Identity and Access Management',
+  },
+  {
+    provider: 'Platforms - Payment',
+    name: 'Tu Duong-Witte',
+    designation: 'Director, Common Platforms',
+  },
+];
+
+export const sideNavItems = [
+  {
+    title: 'Enterprise Platform Team',
+    id: 'enterprise-platform-team',
+  },
+  {
+    title: 'Digital Design and Delivery',
+    id: 'digital-design-and-delivery',
+  },
+  {
+    title: 'Portfolio Platform Team',
+    id: 'portfolio-platform-team',
+  },
+  {
+    title: 'Product team',
+    id: 'product-team',
+  },
+  {
+    title: 'Product',
+    id: 'product',
+  },
+];
