9b2e9eb5200b77582aef9edbe36b21dcf568defc
subbaramaiah mettu <subbumettu007@gmail.com>
2024-06-12 14:50:16 -0600

    feat: skeleton pages for DS app

---
+++ apps/digital-strategy/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  GoAAppFooter,
+  GoAThreeColumnLayout
+} from '@abgov/react-components-4.20.2';
+
+const Footer = () => {
+  return (
+    <>
+      <GoAAppFooter maxContentWidth="1500px">
+      </GoAAppFooter>
+    </>
+  );
+};
+
+export default Footer;
