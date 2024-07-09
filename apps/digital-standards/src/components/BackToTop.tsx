98e9dd28ef68973892a1cfbc8a858101026423bb
Steve Rozeboom <steve.rozeboom@gov.ab.ca>
2024-07-09 12:04:16 -0600

    Changed "Back to top" to a <GoAButton>

--- apps/digital-standards/src/components/BackToTop.tsx
+++ apps/digital-standards/src/components/BackToTop.tsx
@@ -1,4 +1,4 @@
-import { GoAIcon } from '@abgov/react-components-4.20.2';
+import { GoAIcon, GoAButton } from '@abgov/react-components-4.20.2';
 import React from 'react';
 
 const BackToTop = () => {
@@ -6,11 +6,12 @@ const BackToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
   return (
-    <div className='back-top'>
-    <a onClick={scrollToTop} className='back-top-text'>Back to top</a>
-    <a onClick={scrollToTop}><GoAIcon type="arrow-up-circle" theme="outline" /> </a>
-    </div>
-  );
+    <div className='back-top'>    
+    <GoAButton type="tertiary" size="compact" trailingIcon="arrow-up-circle" onClick={scrollToTop}>
+    Back to top
+    </GoAButton>    
+    </div>    
+  );  
 };
 
 export default BackToTop;
