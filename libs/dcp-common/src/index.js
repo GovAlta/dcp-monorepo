acca19f73bbffa10137f0827aa6619cf50763b18
subbaramaiah mettu <subbumettu007@gmail.com>
2024-08-15 15:10:21 -0600

    feat: moved search into a reusable component

--- libs/dcp-common/src/index.js
+++ libs/dcp-common/src/index.js
@@ -1,2 +1,3 @@
 export { default as DcpCommon } from './lib/DcpCommon.astro';
 export { default as DefaultMicrositeLayout } from './lib/layouts/DefaultMicrositeLayout.astro';
+export { default as Search } from './lib/components/Search.astro';
