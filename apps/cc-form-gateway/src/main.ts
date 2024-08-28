a9855a32317f1886a35f65438dc55b210e945e1a
smettu007 <subbumettu007@gmail.com>
2024-08-28 00:18:15 -0600

    fix: added cors to cc-form-gateway app

--- apps/cc-form-gateway/src/main.ts
+++ apps/cc-form-gateway/src/main.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import * as cors from 'cors';
 
 const host = process.env.HOST ?? 'localhost';
 // const port = process.env.PORT ? Number(process.env.PORT) : 33;
 const port = 3333;
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
+});
+
+app.get('/health', (req, res) => {
+  res.send({ message: 'healthy API' });
 });
 
 app.listen(port, host, () => {
