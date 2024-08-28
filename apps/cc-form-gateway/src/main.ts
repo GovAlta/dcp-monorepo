b443df33e0e7c4c0f160f2934cc2434425aff7aa
Subbu <subbumettu007@gmail.com>
2024-08-27 19:21:50 -0600

    feat: added node-express support to dcp and temp disable lint, test (#155)
    * feat: added node-express support to dcp
    
    * fix: updated port no
    
    * fix: skip lint temporarily
    
    * fix: temp disable test

---
+++ apps/cc-form-gateway/src/main.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const host = process.env.HOST ?? 'localhost';
+// const port = process.env.PORT ? Number(process.env.PORT) : 33;
+const port = 3333;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
