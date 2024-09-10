314582de76df7ee61d5f5d69467636ae51776f0e
smettu007 <subbumettu007@gmail.com>
2024-09-09 23:09:55 -0600

    feat: added configmap and logged values

--- apps/digital-marketplace-api/src/main.ts
+++ apps/digital-marketplace-api/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { environment } from './environment';
+
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
@@ -13,4 +15,11 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`[ ready ] ${port}`);
+  console.log(`[ env ] KEYCLOAK_ROOT_URL: ${environment.KEYCLOAK_ROOT_URL}`);
+  console.log(`[ env ] REALM: ${environment.REALM}`);
+  console.log(`[ env ] CLIENT_ID: ${environment.CLIENT_ID}`);
+  console.log(`[ env ] CLIENT_SECRET: ${environment.CLIENT_SECRET}`);
+  console.log(`[ env ] LOG_LEVEL: ${environment.LOG_LEVEL}`);
+  console.log(`[ env ] PORT: ${environment.PORT}`);
+  console.log(`[ env ] DIRECTORY_URL: ${environment.DIRECTORY_URL}`);
 });
