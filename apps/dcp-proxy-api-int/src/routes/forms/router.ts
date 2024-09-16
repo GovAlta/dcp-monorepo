2e70a1c13bc5da67e1bf660e493a61e275ad527e
smettu007 <subbumettu007@gmail.com>
2024-09-16 12:46:40 -0600

    feat; added supplier and partner data

--- apps/dcp-proxy-api-int/src/routes/forms/router.ts
+++ apps/dcp-proxy-api-int/src/routes/forms/router.ts
@@ -88,6 +88,165 @@ export function downloadBuyerFormData(
   };
 }
 
+export function downloadSupplierFormData(
+  logger: Logger,
+  formApiUrl: URL,
+  tokenProvider: TokenProvider
+): RequestHandler {
+  return async (req, res) => {
+    try {
+      const token = await tokenProvider.getAccessToken();
+
+      const submittedSupplierForms = await axios.get(
+        `${formApiUrl}/forms?criteria={"definitionIdEquals":"supplier-form"}&top=5000`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const submittedForms = submittedSupplierForms.data.results;
+      let formDetailsPromises: Promise<any>[] = [];
+      const formDetails: any[] = [];
+      for (const form of submittedForms) {
+        formDetailsPromises.push(
+          axios
+            .get(`${formApiUrl}/forms/${form.id}/data`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            })
+            .then((response) => response.data)
+        );
+
+        if (formDetailsPromises.length >= 10) {
+          const responses = await Promise.all(formDetailsPromises);
+          formDetails.push(...responses);
+          formDetailsPromises = [];
+        }
+      }
+      if (formDetailsPromises.length > 0) {
+        const responses = await Promise.all(formDetailsPromises);
+        formDetails.push(...responses);
+      }
+
+      const headers = [
+        'email',
+        'role',
+        'agreement',
+        'first-name',
+        'last-name',
+        'interest',
+        'org-name',
+        'website',
+        'head-office',
+        'sector'
+      ];
+  
+      const csv = [headers.map((header) => `"${header.replace(/"/g, '""')}"`).join(",")].concat(
+        formDetails.map((form) => {
+          return headers.map((header) => {
+            const value = form.data[header];
+            return `"${String(value).replace(/"/g, '""').replace(/,/g, '\,')}"`;
+          }).join(",");
+        })
+      );
+
+      res.setHeader('Content-Disposition', 'attachment; filename="forms.csv"');
+      res.setHeader('Content-Type', 'text/csv');
+      res.send(Buffer.from(csv.join("\n")));
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        res.status(e.response.status).send(e.response.data);
+        logger.error(e.response.data, 'Failed to download supplier form data');
+      } else {
+        res.status(400).send({ error: e });
+        logger.error(e, 'Failed to download supplier form data');
+      }
+    }
+  };
+}
+
+export function downloadPartnerFormData(
+  logger: Logger,
+  formApiUrl: URL,
+  tokenProvider: TokenProvider
+): RequestHandler {
+  return async (req, res) => {
+    try {
+      const token = await tokenProvider.getAccessToken();
+
+      const submittedPartnerForms = await axios.get(
+        `${formApiUrl}/forms?criteria={"definitionIdEquals":"partner-form"}&top=5000`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const submittedForms = submittedPartnerForms.data.results;
+      let formDetailsPromises: Promise<any>[] = [];
+      const formDetails: any[] = [];
+      for (const form of submittedForms) {
+        formDetailsPromises.push(
+          axios
+            .get(`${formApiUrl}/forms/${form.id}/data`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            })
+            .then((response) => response.data)
+        );
+
+        if (formDetailsPromises.length >= 10) {
+          const responses = await Promise.all(formDetailsPromises);
+          formDetails.push(...responses);
+          formDetailsPromises = [];
+        }
+      }
+      if (formDetailsPromises.length > 0) {
+        const responses = await Promise.all(formDetailsPromises);
+        formDetails.push(...responses);
+      }
+
+      const headers = [
+        'email',
+        'role',
+        'agreement',
+        'first-name',
+        'last-name',
+        'interest',
+        'org-name',
+        'website',
+        'head-office',
+      ];
+  
+      const csv = [headers.map((header) => `"${header.replace(/"/g, '""')}"`).join(",")].concat(
+        formDetails.map((form) => {
+          return headers.map((header) => {
+            const value = form.data[header];
+            return `"${String(value).replace(/"/g, '""').replace(/,/g, '\,')}"`;
+          }).join(",");
+        })
+      );
+
+      res.setHeader('Content-Disposition', 'attachment; filename="forms.csv"');
+      res.setHeader('Content-Type', 'text/csv');
+      res.send(Buffer.from(csv.join("\n")));
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        res.status(e.response.status).send(e.response.data);
+        logger.error(e.response.data, 'Failed to download supplier form data');
+      } else {
+        res.status(400).send({ error: e });
+        logger.error(e, 'Failed to download supplier form data');
+      }
+    }
+  };
+}
+
 export function createFormsRouter({
   logger,
   tokenProvider,
@@ -98,6 +257,14 @@ export function createFormsRouter({
   router.get(
     '/forms/buyer/downloadcsv',
     downloadBuyerFormData(logger, formApiUrl, tokenProvider)
+  );
+  router.get(
+    '/forms/supplier/downloadcsv',
+    downloadSupplierFormData(logger, formApiUrl, tokenProvider)
+  );
+  router.get(
+    '/forms/partner/downloadcsv',
+    downloadPartnerFormData(logger, formApiUrl, tokenProvider)
   );
 
   return router;
--- apps/digital-marketplace-int/src/pages/landing/index.tsx
+++ apps/digital-marketplace-int/src/pages/landing/index.tsx
@@ -1,44 +1,67 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import './styles.css'
+
+const downloadFormData = async (url: string, fileName: string) => {
+  const download = await axios.get(url);
+  const blob = new Blob([download.data], { type: 'text/csv' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = fileName;
+  link.click();
+  URL.revokeObjectURL(link.href);
+};
 
 export default function LandingPage() {
-  const [buyerDataUrl, setBuyerDataUrl] = React.useState('');
   const [isLoading, setIsLoading] = React.useState(false);
 
-  useEffect(() => {
+  const getDownloadUrl = (role: 'buyer' | 'supplier' | 'partner') => {
     if (
       window.location.hostname ===
         'digital-marketplace-int-dcp-uat.apps.aro.gov.ab.ca' ||
       window.location.hostname === 'localhost'
     ) {
-      setBuyerDataUrl(
-        'https://dcp-proxy-api-int-dcp-uat.apps.aro.gov.ab.ca/marketplace/v1/forms/buyer/downloadcsv'
-      ); // uat url
+      // return `http://localhost:3333/marketplace/v1/forms/${role}/downloadcsv`;
+      return `https://dcp-proxy-api-int-dcp-uat.apps.aro.gov.ab.ca/marketplace/v1/forms/${role}/downloadcsv`;
     } else {
-      setBuyerDataUrl(
-        'https://dcp-proxy-api-int-dcp-prod.apps.aro.gov.ab.ca/marketplace/v1/forms/buyer/downloadcsv'
-      );
+      return `https://dcp-proxy-api-int-dcp-prod.apps.aro.gov.ab.ca/marketplace/v1/forms/${role}/downloadcsv`;
     }
-  }, []);
+  };
 
-  const downloadBuyerFormData = async () => {
+  const downloadFormDataByRole = async (role: 'buyer' | 'supplier' | 'partner') => {
     setIsLoading(true);
-    const download = await axios.get(buyerDataUrl);
-    const blob = new Blob([download.data], { type: 'text/csv' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'buyer_data.csv';
-    a.click();
-    URL.revokeObjectURL(url);
+    await downloadFormData(getDownloadUrl(role), `${role}_data.csv`);
     setIsLoading(false);
   };
+
   return (
-    <div>
+    <div className="landing-page">
       <h1>Digital Marketplace portal</h1>
-      <button disabled={isLoading} onClick={() => downloadBuyerFormData()}>
-        Download buyer data
-      </button>
+      <div className="buttons">
+        <button
+          disabled={isLoading}
+          className="button"
+          onClick={() => downloadFormDataByRole('buyer')}
+        >
+          Download buyer data
+        </button>
+        <button
+          disabled={isLoading}
+          className="button"
+          onClick={() => downloadFormDataByRole('supplier')}
+        >
+          Download supplier data
+        </button>
+        <button
+          disabled={isLoading}
+          className="button"
+          onClick={() => downloadFormDataByRole('partner')}
+        >
+          Download partner data
+        </button>
+      </div>
     </div>
   );
 }
+
+
