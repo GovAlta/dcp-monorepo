5c5be01edccb6a74c456d64e70265ce7e69977c9
mxaviers <mxaviers@users.noreply.github.com>
2024-11-14 17:31:46 -0700

    feat: DM: Addition of new components, new Understanding Procurement page, graph… (#201)
    * Addition of new components, new Understanding Procurement page, graphics, and adjustments to adm-scripts JS and core.css
    
    * Made adjustments from initial review.
    
    * Back to Top adjustment
    
    * Separated CSS from Understanding Procurement into own file plus general CSS changes.
    
    ---------
    
    Co-authored-by: miguel-soco_goa <youremail@gov.ab.ca>

--- apps/digital-marketplace/public/adm-scripts.js
+++ apps/digital-marketplace/public/adm-scripts.js
@@ -12,40 +12,40 @@ window.addEventListener('load', function () {
             }
         };
     };
-}, false);
 
-/* Mobile Menu Toggle ****/
+    /* Mobile Menu Toggle ****/
 
-var mobileBtn = document.querySelector('button.goa-adm-button');
-var mobileMenu = document.querySelector('nav.goa-adm-nav-menu');
+    var mobileBtn = document.querySelector('button.goa-adm-button');
+    var mobileMenu = document.querySelector('nav.goa-adm-nav-menu');
 
-mobileBtn.onclick = function () {
-    if (mobileBtn.getAttribute('aria-expanded') == 'true'){ 
-        mobileBtn.setAttribute('aria-expanded', 'false');
-        mobileMenu.classList.remove('mobile-active');
-    } else {
-        mobileBtn.setAttribute('aria-expanded', 'true');
-        mobileMenu.classList.add('mobile-active');
-    }
-    return false;
-};
-
-window.addEventListener('resize', function () {
-    var viewWidth = getViewportWidth();
-    if (viewWidth > 900) {
-        if (mobileMenu.className.indexOf('mobile-active') != -1) {
-            mobileMenu.classList.remove('mobile-active');
+    mobileBtn.onclick = function () {
+        if (mobileBtn.getAttribute('aria-expanded') == 'true'){ 
             mobileBtn.setAttribute('aria-expanded', 'false');
+            mobileMenu.classList.remove('mobile-active');
+        } else {
+            mobileBtn.setAttribute('aria-expanded', 'true');
+            mobileMenu.classList.add('mobile-active');
+        }
+        return false;
+    };
+
+    window.addEventListener('resize', function () {
+        var viewWidth = getViewportWidth();
+        if (viewWidth > 1080) {
+            if (mobileMenu.className.indexOf('mobile-active') != -1) {
+                mobileMenu.classList.remove('mobile-active');
+                mobileBtn.setAttribute('aria-expanded', 'false');
+            };
         };
+    }, false);
+
+
+    /* getViewportWidth **********************************************/
+
+    function getViewportWidth() {
+        return (window.innerWidth ? window.innerWidth : (document.documentElement && document.documentElement.clientWidth ? document.documentElement.clientWidth : (document.body && document.body.clientWidth ? document.body.clientWidth : 0)));
     };
 }, false);
-
-
-/* getViewportWidth **********************************************/
-
-function getViewportWidth() {
-    return (window.innerWidth ? window.innerWidth : (document.documentElement && document.documentElement.clientWidth ? document.documentElement.clientWidth : (document.body && document.body.clientWidth ? document.body.clientWidth : 0)));
-};
 
 window.onload = function() {
     var body = document.querySelector('body');
---
+++ apps/digital-marketplace/src/components/accordion/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './styles.css';
+import open_icon from '../../img/goa-accordion-open-icon.svg';
+import close_icon from '../../img/goa-accordion-close-icon.svg';
+
+interface AccordionProps {
+  title: string;
+  content: any;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="goa-adm-accordion-item">
+      <button className="goa-adm-accordion-title" aria-expanded={isActive ? 'true' : 'false'} onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div className="goa-adm-accordion-icon"><img src={isActive ? close_icon : open_icon} alt={isActive ? "Close accordion panel" : "Open accordion panel"} /></div>
+      </button>
+      <div className={isActive ? "goa-adm-accordion-content expanded" : "goa-adm-accordion-content"}>{content}</div>
+    </div>
+  );
+};
+
+export default Accordion;
