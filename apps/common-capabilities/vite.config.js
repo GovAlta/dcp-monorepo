48eb899c75e0240e2fb92caae6f37689e6ac462a
hwen3 <jim168825@gmail.com>
2025-01-31 15:59:58 -0700

    chore: build warning

--- apps/common-capabilities/vite.config.js
+++ apps/common-capabilities/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  build: {
+    emptyOutDir: true
+  },
   plugins: [react(), nxViteTsPaths()],
 });
