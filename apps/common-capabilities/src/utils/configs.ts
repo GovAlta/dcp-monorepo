59d21ce02f1b92a05d7176fe36ffd082a00ccc07
Jim Wen <Han.Wen+goa@gov.ab.ca>
2025-02-18 14:28:10 -0700

    fix: addressing issue with url parser in prod

--- apps/common-capabilities/src/utils/configs.ts
+++ apps/common-capabilities/src/utils/configs.ts
@@ -72,13 +72,15 @@ const serviceConfigs: ServiceConfig = {
 
 function getEnv() {
     const url = window.location.hostname.toLowerCase();
-    const match = url.match(/([a-z0-9-]+)-([a-z]+)\.([a-z0-9-]+)/);
-    const env = match ? match[2] : null;
+    const match = url.match(/common-capabilities-dcp-(\w+)\.apps\.aro\.gov\.ab\.ca/);
+    const env = match ? match[1] : null;
 
     if (!env) {
       if (url === 'localhost') {
         return Environment.uat;
-      } else if (url === 'common-capabilities.digital.gov.ab.ca') {
+      } else if (url === 'common-capabilities-uat.alberta.ca') {
+        return Environment.uat;
+      } else if (url === 'common-capabilities.digital.gov.ab.ca' || url === 'common-capabilities.gov.ab.ca') {
         return Environment.prod;
       }
     }
