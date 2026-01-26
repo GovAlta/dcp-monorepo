b6b44be74b9a80fc3a011b66090f06412e3982c7
smettu007 <subbumettu007@gmail.com>
2026-01-26 08:52:07 -0700

    fix: build failurea

--- apps/digital-standards/src/layouts/Layout/index.tsx
+++ apps/digital-standards/src/layouts/Layout/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from '../../components/Header';
+import Header from '../../components/HeaderComponent';
 import {
   GoabThreeColumnLayout,
   GoabSideMenu,
   GoabSideMenuGroup,
   GoabHeroBanner,
 } from '@abgov/react-components';
-import Footer from '../../components/Footer';
+import Footer from '../../components/FooterComponent';
 
 type Props = {
   children: React.ReactNode;
